bbda49e703144d57101ed762d77cd76bd8837e43
Justin Green <justingreen@Justins-MacBook-Air.local>
2024-02-01 09:06:04 -0800

    save

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,11 +6,13 @@ import {FaCodepen} from "react-icons/fa"
 const Header = () => {
   return (
     <div className="header">
-      {/* <div> */}
+
+
+      {/* NAME-ROW */}
         <div className="name-row">
                 <div className="firstname">
                     <div className="hidden-overflow">
-                    <h1 className="name-h1 bold large drop-in delay-3">J</h1>
+                        <h1 className="name-h1 bold large drop-in delay-3">J</h1>
                     </div>
                     <h1 className="name-h1 mid-thin rotate-focus delay-1">u</h1>
                     <h1 className="name-h1 mid-thin rotate-focus delay-2">s</h1>
@@ -20,7 +22,7 @@ const Header = () => {
                 </div>
                 <div className="lastname">
                     <div className="hidden-overflow">
-                    <h1 className="name-h1 bold large drop-in delay-3">G</h1>
+                        <h1 className="name-h1 bold large drop-in delay-3">G</h1>
                     </div>
                     <h1 className="name-h1 mid-thin rotate-focus delay-1">r</h1>
                     <h1 className="name-h1 mid-thin rotate-focus delay-2">e</h1>
@@ -28,60 +30,59 @@ const Header = () => {
                     <h1 className="name-h1 mid-thin rotate-focus delay-4">n</h1>
                 </div>
             </div>
+
             <div className="header-details">
-            <div>
-            <div className="description-row">
-                <h4 className="mid-thin rise-up delay-4">Front-end developer with <span className="bold rise-up">full</span> stack<strong>+</strong> knowledge</h4>
-            </div>
-            <div className="blurb-div">
-                <h5 className="thin scalegrow delay-6">I build pixel-<strong>perfect</strong>, engaging, and accessible sites.</h5>
-            </div>
-            </div>
-            <ul className="skills">
-                <li className="skill-item slide-right delay-1">
-                    <h3 className="thin">Newbie</h3>
-                </li>
-                <li className="skill-item slide-right delay-2">
-                <h3 className="bold">Beginner</h3>
+                <div class="header-blurb-description-div">
+                    <div className="description-row">
+                        <h4 className="mid-thin rise-up delay-4">Front-end developer with <span className="bold rise-up">full</span> stack<strong>+</strong> knowledge</h4>
+                    </div>
+                    <div className="blurb-div">
+                        <h5 className="thin scalegrow delay-6">I build pixel-<strong>perfect</strong>, engaging, and accessible sites.</h5>
+                    </div>
+                </div>
+                <ul className="skills">
+                    <li className="skill-item slide-right delay-1">
+                        <h3 className="thin">Newbie</h3>
+                    </li>
+                    <li className="skill-item slide-right delay-2">
+                        <h3 className="bold">Beginner</h3>
+                    </li>
+                    <li className="skill-item slide-right delay-3">
+                        <h3 className="thin">Junior</h3>
+                    </li>
+                    <li className="skill-item slide-right delay-4">
+                        <h3 className="thin">Senior</h3>
+                    </li>
+                </ul>
 
-                </li>
-                <li className="skill-item slide-right delay-3">
-                    <h3 className="thin">Junior</h3>
-                </li>
-                <li className="skill-item slide-right delay-4">
-                    <h3 className="thin">Senior</h3>
-                </li>
-            </ul>
-        
-
-        <ul className="links">
-            <li className="link-item drop-in delay-0">
-                <a target="_blank" href="https://www.linkedin.com/in/justin-green-2484513b/">
-                    <AiOutlineLinkedin className="media-icon"/>
-                </a>
-            </li>
-            <li className="link-item drop-in delay-1">
-                <a target="_blank" href="https://github.com/jgreen721">
-                    <AiOutlineGithub className="media-icon"/>
-                </a>
-            </li>
-            <li className="link-item drop-in delay-2">
-                <a target="_blank" href="https://codepen.io/jgreen721">
-                    <FaCodepen className="media-icon"/>
-                </a>
-            </li>
-            <li className="link-item drop-in delay-3">
-                <a target="_blank" href="https://www.instagram.com/justng2d1/">
-                <AiOutlineInstagram className="media-icon"/>
-                </a>
-            </li>
-            <li className="link-item drop-in delay-4">
-                <a target="_blank" href="https://www.frontendmentor.io/profile/jgreen721">
-                    <AiOutlineLaptop className="media-icon"/>
-                </a>
-            </li>
-            </ul>
-        </div>
+                 <ul className="links">
+                    <li className="link-item drop-in delay-0">
+                        <a target="_blank" href="https://www.linkedin.com/in/justin-green-2484513b/">
+                            <AiOutlineLinkedin className="media-icon"/>
+                        </a>
+                    </li>
+                    <li className="link-item drop-in delay-1">
+                        <a target="_blank" href="https://github.com/jgreen721">
+                            <AiOutlineGithub className="media-icon"/>
+                        </a>
+                    </li>
+                    <li className="link-item drop-in delay-2">
+                        <a target="_blank" href="https://codepen.io/jgreen721">
+                            <FaCodepen className="media-icon"/>
+                        </a>
+                    </li>
+                    <li className="link-item drop-in delay-3">
+                        <a target="_blank" href="https://www.instagram.com/justng2d1/">
+                            <AiOutlineInstagram className="media-icon"/>
+                        </a>
+                    </li>
+                    <li className="link-item drop-in delay-4">
+                        <a target="_blank" href="https://www.frontendmentor.io/profile/jgreen721">
+                            <AiOutlineLaptop className="media-icon"/>
+                        </a>
+                    </li>
+                </ul>
+            </div>
     </div>
   )
 }
