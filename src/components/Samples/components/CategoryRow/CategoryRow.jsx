d73f139bf3ed0eda9e23378953c9e2f07faec6d3
Justin Green <justingreen@Justins-MacBook-Air.local>
2024-02-01 17:14:59 -0800

    save

--- src/components/Samples/components/CategoryRow/CategoryRow.jsx
+++ src/components/Samples/components/CategoryRow/CategoryRow.jsx
@@ -1,6 +1,8 @@
 import React, {useState, useRef, useEffect} from 'react'
 import { useObserver } from '../../../../hooks/useObserver';
 import {useAppContext} from "../../../../context/AppContext"
+import {FaReact,FaHtml5,FaCss3, FaJs, FaArrowLeft, FaArrowRight} from "react-icons/fa"
+import {TbBrandBlender,TbBrandThreejs} from "react-icons/tb"
 import "./CategoryRow.css"
 
 const CategoryRow = ({category}) => {
@@ -8,6 +10,7 @@ const CategoryRow = ({category}) => {
   const categoryRef = useRef();
   const {isVisible} = useObserver(categoryRef)
   const {updateCategory} = useAppContext()
+  const [info,setInfo] = useState({})
 
   useEffect(()=>{
     console.log('isVisible',isVisible);
@@ -16,6 +19,33 @@ const CategoryRow = ({category}) => {
     }
   })
 
+  const handleSetInfo=(info)=>{
+    let icons = [];
+    info.technology.forEach(item=>{
+      if(item == "HTML" || item.name == "HTML"){
+        icons.push(({name:"HTML",icon:<FaHtml5/>,color:"html_orange"}))
+      }
+      if(item == "JS" || item.name == "JS"){
+        icons.push(({name:"JS",icon:<FaJs/>,color:"js_yellow"}))
+      }
+      if(item == "CSS" || item.name == "CSS"){
+        icons.push(({name:"CSS",icon:<FaCss3/>,color:"css_blue"}))
+      }
+      if(item == "React" || item.name == "React"){
+        icons.push(({name:"React",icon:<FaReact/>,color:"react_blue"}))
+      }
+      if(item == "Blender" || item.name == "Blender"){
+        icons.push(({name:"Blender",icon:<TbBrandBlender/>,color:"blender_orange"}))
+      }
+      if(item == "Three" || item.name == "Three"){
+        icons.push(({name:"Three",icon:<TbBrandThreejs/>,color:"three_grey"}))
+      }
+    })
+    let temp = info;
+    temp.technology = icons;
+    setInfo(temp);
+  }
+
   // console.log(setData)
   return (
     <div ref={categoryRef} className="category-row-section">
@@ -23,15 +53,35 @@ const CategoryRow = ({category}) => {
         <h5 className="mid-thin">{category.description}</h5>
         <ul className="carousel">
           <div className="carousel-row">
-        {data.map((item)=>(
-<li key={item.id} className={item.translate == -105 || item.translate == 210 ?  "carousel-item none" : "carousel-item"} style={{transform:item.translate ? `translateX(${item.translate}%)` : 'none'}}>
-    <img className="carousel-img" src={item.img}/>
-</li>
-        ))}
-        </div>
+            {data.map((item)=>(
+              <li onClick={()=>handleSetInfo(item)} key={item.id} className={item?.hide == true ?  "carousel-item hide-item" : "carousel-item"} style={{transform:`translateX(${item.translate}%)`}}>
+                <img className="carousel-img" src={item.img}/>
+              </li>
+              ))}
+          <button className="carousel-btn prev-btn" onClick={()=>setData((data)=>data.map(d=>d.translate > -104 ? {...d,hide:false,translate:d.translate - 105} : {...d,hide:true,translate:210}))}><FaArrowLeft/></button>
+          <button className="carousel-btn next-btn" onClick={()=>setData((data)=>data.map(d=>d.translate < 209 ? {...d,hide:false,translate:d.translate + 105} : {...d,hide:true,translate:-105}))}><FaArrowRight/></button>
+          </div>
+
         </ul>
-        <button className="carousel-btn" onClick={()=>setData((data)=>data.map(d=>d.translate > -105 ? {...d,translate:d.translate - 105} : {...d,translate:210}))}>Prev</button>
-        <button className="carousel-btn" onClick={()=>setData((data)=>data.map(d=>d.translate < 210 ? {...d,translate:d.translate + 105} : {...d,translate:-105}))}>Next</button>
+        <div className="category-panel-row">
+          {/* <button className="carousel-btn" onClick={()=>setData((data)=>data.map(d=>d.translate > -105 ? {...d,translate:d.translate - 105} : {...d,translate:210}))}>Prev</button>
+          <button className="carousel-btn" onClick={()=>setData((data)=>data.map(d=>d.translate < 210 ? {...d,translate:d.translate + 105} : {...d,translate:-105}))}>Next</button> */}
+          <div className="preview-container">
+            {!info?.title && <h4 className="empty-h4">Select a Sample</h4>}
+            {info?.title &&
+              <div className="item-info">
+                 <h5 className="mid-thin">{info?.title}</h5>
+                 <ul className="technology-list">
+                  {info.technology.map(t=>(
+                    <h4 className={t.color} key={t.name}>{t.icon} </h4>
+
+                  ))}
+                  </ul>
+                  <p>{info.description}</p>
+                  <a className="visit-link" target="_blank" href={info.site}>Visit</a>
+              </div>}
+          </div>
+        </div>
 
     </div>
   )
--- src/data.jsx
+++ src/data.jsx
@@ -8,10 +8,10 @@ export const data = [
       level:"Newbie",
       description:"Getting your feet wet with HTML, CSS and a bit of Javascript in building small single components.",
       samples:[
-        {id:1,title:"FAQ-Card",img:faqCard,site:"https://frontend3dsolution.vercel.app/",translate:-105},
-        {id:2,title:"NFT-Card",img:nftCard,site:"https://jgreen721.github.io/frontend_challenges/nftfigma/",translate:0},
-        {id:3,title:"Perfume Ad",img:perfumeAd,site:"https://perfume3d.vercel.app/",translate:105},
-        {id:4,title:"Survey Card",img:resultsCard,site:"https://jgreen721.github.io/frontend_challenges/results_summary/",translate:210},
+        {id:1,title:"FAQ-Card",description:"A spruced up faq card with drop-down behavior, a running carousel and staggered intro animation for an enhanced UX.",technology:["HTML","CSS","ThreeJS","React","Blender"],img:faqCard,site:"https://frontend3dsolution.vercel.app/",translate:-105,code:"https://github.com/jgreen721/frontend3dsolution"},
+        {id:2,title:"NFT-Card",description:"A centered NFT card with hover effects.",technology:["HTML","CSS"],img:nftCard,site:"https://jgreen721.github.io/frontend_challenges/nftfigma/",translate:0,code:"https://github.com/jgreen721/frontend_challenges/tree/master/nftfigma"},
+        {id:3,title:"Perfume Ad",description:"An animated perfume-ad card with staggered animation, as well as a 3d solution with custom gltf models and a camera panning intro.",technology:["HTML","CSS","ThreeJS","React","Blender"],img:perfumeAd,site:"https://perfume3d.vercel.app/",translate:105,site:"",code:"https://github.com/jgreen721/perfume3d"},
+        {id:4,title:"Survey Card",description:"A results summary card with theme-toggles and JS that measures the spoofed data and determine to invoke confetti via 3rd party lib",technology:["HTML","CSS","JS"],img:resultsCard,site:"https://jgreen721.github.io/frontend_challenges/results_summary/",translate:210,site:"",code:"https://github.com/jgreen721/frontend_challenges/tree/master/results_summary"},
       ]
     },
     {
@@ -19,10 +19,10 @@ export const data = [
       level:"Beginner",
       description:"Building out small sites and applying responsive positioning to page elements.",
       samples:[
-        {id:1,title:"Maker Prelaunch",img:makerPrelaunch,site:"https://jgreen721.github.io/frontend_challenges/maker-prelaunch/",translate:-105},
-        {id:2,title:"MultistepForm",img:multistepForm,site:"https://do-multistepform-dcdfuwwgc-jgreen721.vercel.app/",translate:0},
-        {id:3,title:"Notifications Snippet",img:notifications,site:"https://jgreen721.github.io/frontend_challenges/notificationsapp/",translate:105},
-        {id:4,title:"Password Generator",img:pwordGenerator,site:"https://jgreen721.github.io/frontend_challenges/passwd_gen/",translate:210},
+        {id:1,title:"Maker Prelaunch",description:"",technology:["HTML","CSS","JS"],img:makerPrelaunch,site:"https://jgreen721.github.io/frontend_challenges/maker-prelaunch/",translate:-105},
+        {id:2,title:"MultistepForm",description:"",technology:["HTML","CSS","JS"],img:multistepForm,site:"https://do-multistepform-dcdfuwwgc-jgreen721.vercel.app/",translate:0},
+        {id:3,title:"Notifications Snippet",description:"",technology:["HTML","CSS","JS"],img:notifications,site:"https://jgreen721.github.io/frontend_challenges/notificationsapp/",translate:105},
+        {id:4,title:"Password Generator",description:"",technology:["HTML","CSS","JS"],img:pwordGenerator,site:"https://jgreen721.github.io/frontend_challenges/passwd_gen/",translate:210},
       ]
     },
     {
@@ -30,10 +30,10 @@ export const data = [
       level:"Junior",
       description:"Continuing on with more advanced designs and deeper functionality",
       samples:[
-        {id:1,title:"Body Mass Calculator",img:bodyMass,site:"https://frontend3dsolution.vercel.app/",translate:-105},
-        {id:2,title:"Clock UI",img:clockSite,site:"https://clockapp-react.vercel.app/",translate:0},
-        {id:3,title:"Portfolio Demo",img:portfolio,site:"https://portfolio-challenge-three.vercel.app/",translate:105},
-        {id:4,title:"Programming Quiz",img:quiz,site:"https://jgreen721.github.io/frontend_challenges/results_summary/",translate:210},
+        {id:1,title:"Body Mass Calculator",description:"",technology:["HTML","CSS","React"],img:bodyMass,site:"https://frontend3dsolution.vercel.app/",translate:-105},
+        {id:2,title:"Clock UI",description:"",technology:["HTML","CSS","JS"],img:clockSite,site:"https://clockapp-react.vercel.app/",translate:0},
+        {id:3,title:"Portfolio Demo",description:"",technology:["HTML","CSS","JS"],img:portfolio,site:"https://portfolio-challenge-three.vercel.app/",translate:105},
+        {id:4,title:"Programming Quiz",description:"",technology:["HTML","CSS","JS"],img:quiz,site:"https://jgreen721.github.io/frontend_challenges/results_summary/",translate:210},
       ]
     },
     {
@@ -41,10 +41,10 @@ export const data = [
       level:"Senior",
       description:"Fully functional sites ready for a backend server and to go into production.",
       samples:[
-        {id:1,title:"Movie Site",img:movieSite,site:"https://fe-entertainmentwebapp.vercel.app/",translate:-105},
-        {id:2,title:"Task Manager",img:taskManager,site:"https://react-feedbackmanager.vercel.app/",translate:0},
-        {id:3,title:"SpeakerSite",img:perfumeAd,site:"https://perfume3d.vercel.app/",translate:105},
-        {id:4,title:"Survey Card",img:resultsCard,site:"https://jgreen721.github.io/frontend_challenges/results_summary/",translate:210},
+        {id:1,title:"Movie Site",description:"",technology:["HTML","CSS","React"],img:movieSite,site:"https://fe-entertainmentwebapp.vercel.app/",translate:-105},
+        {id:2,title:"Task Manager",description:"",technology:["HTML","Tailwind","React"],img:taskManager,site:"https://react-feedbackmanager.vercel.app/",translate:0},
+        {id:3,title:"SpeakerSite",description:"",technology:["HTML","CSS","JS"],img:perfumeAd,site:"https://perfume3d.vercel.app/",translate:105},
+        {id:4,title:"Survey Card",description:"",technology:["HTML","CSS","JS"],img:resultsCard,site:"https://jgreen721.github.io/frontend_challenges/results_summary/",translate:210},
       ]
     },
   ]
